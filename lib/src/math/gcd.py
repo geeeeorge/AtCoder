1d507843a4e6f9f05428b4dbc4254236b0b39c80
geeeeorge <yuto.george@i.softbank.jp>
2023-04-29 20:38:52 +0900

    最大公約数

---
+++ lib/src/math/gcd.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    """
+    最大公約数を求める
+    Greatest Common Divisor
+    """
+    while a >= 1 and b >= 1:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a if a != 0 else b
