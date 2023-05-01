d29a20267cd606a2c605c3d7b8e51c9294ac7cec
geeeeorge <yuto.george@i.softbank.jp>
2023-05-02 07:04:46 +0900

    solve: tessoku A40

---
+++ atcoder/kyopro_tessoku/A/A40.py
@@ -0,0 +1,42 @@
+N = int(input())
+A = [0] * 109
+
+B = map(int, input().split())
+for b in B:
+    A[b] += 1
+
+
+def combination(n, r, m=10 ** 9 + 7):
+    def pow(a, n, mod):
+        res = 1
+        while n > 0:
+            if n & 1:
+                res = res * a % mod
+            a = a * a % mod
+            n >>= 1
+        return res
+
+    # 分子aを求める
+    a = 1
+    for i in range(n):
+        a = a * (i + 1) % m
+
+    # 分母bを求める
+    b = 1
+    for i in range(r):
+        b = b * (i + 1) % m
+    for i in range(n - r):
+        b = b * (i + 1) % m
+
+    # bの逆元を求める
+    b_inv = pow(b, m - 2, m)
+    return a * b_inv % m
+
+
+ans = 0
+for i in range(1, 101):
+    if A[i] < 3:
+        continue
+
+    ans += combination(A[i], 3)
+print(ans)
