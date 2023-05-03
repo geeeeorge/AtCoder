c4294d9e55dfb5b66f01e6cd753e96a7352274b9
Geeeeorge <yuto.george@icloud.com>
2023-05-03 18:51:32 +0900

    solve: tessoku A42

---
+++ atcoder/kyopro_tessoku/A/A43.py
@@ -0,0 +1,10 @@
+N, L = map(int, input().split())
+ans = 0
+for _ in range(N):
+    a, b = map(str, input().split())
+    a = int(a)
+    if b == 'E':
+        ans = max(ans, L - a)
+    if b == 'W':
+        ans = max(ans, a)
+print(ans)
