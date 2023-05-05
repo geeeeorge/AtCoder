0301c734dc94a6f0d819bf21339906e4b957f096
Geeeeorge <yuto.george@icloud.com>
2023-05-06 07:15:54 +0900

    solve: tessoku A58

---
+++ atcoder/kyopro_tessoku/A/A58.py
@@ -0,0 +1,32 @@
+N, Q = map(int, input().split())
+
+seg = [0] * (1 << 20)
+
+
+# 一点更新
+def update(k, x):
+    k += 1 << 19
+    seg[k] = x
+    while k > 0:
+        k //= 2
+        seg[k] = max(seg[k * 2], seg[k * 2 + 1])
+
+
+# 区間取得
+def get_max(ql, qr, sl=0, sr=1 << 19, k=1):
+    if qr <= sl or sr <= ql:
+        return 0
+    if ql <= sl and sr <= qr:
+        return seg[k]
+    m = (sl + sr) // 2
+    return max(get_max(ql, qr, sl, m, k * 2), get_max(ql, qr, m, sr, k * 2 + 1))
+
+
+for _ in range(Q):
+    cmds = list(map(int, input().split()))
+    if cmds[0] == 1:
+        pos, x = cmds[1:]
+        update(pos, x)
+    else:
+        l, r = cmds[1:]
+        print(get_max(l, r))
