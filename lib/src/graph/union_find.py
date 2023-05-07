7fcc7b794d6f136413139f31d78d94320013214b
geeeeorge <yuto.george@i.softbank.jp>
2023-05-07 11:01:30 +0900

    UnionFind

--- lib/src/graph/union_find.py
+++ lib/src/graph/union_find.py
@@ -4,9 +4,10 @@ class UnionFind:
     """
     def __init__(self, n):
         import sys
-        sys.setrecursionlimit(10**6)
+        sys.setrecursionlimit(10 ** 6)
 
         self.parent = [-1] * n
+        self.size = [1] * n
 
     def root(self, x: int) -> int:
         if self.parent[x] == -1:
@@ -21,7 +22,13 @@ class UnionFind:
         y = self.root(y)
         if x == y:
             return
-        self.parent[x] = y
+        # マージテク
+        # xの方が大きい
+        if self.size[x] < self.size[y]:
+            x, y = y, x
+        if self.size[x] == self.size[y]:
+            self.size[x] += 1
+        self.parent[y] = x
 
     def is_same(self, x: int, y: int) -> bool:
         return self.root(x) == self.root(y)
